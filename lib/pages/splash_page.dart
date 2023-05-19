ce81c0f1bdc5b690b50aeb61baab34b42bd24c20
Javed Ansari <javeda@thirdrocktechkno.com>
2023-05-19 10:48:28 +0530

    first commit

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:my_chat/constants/color_constants.dart';
+import 'package:my_chat/providers/auth_provider.dart';
+import 'package:provider/provider.dart';
+
+import 'pages.dart';
+
+class SplashPage extends StatefulWidget {
+  SplashPage({super.key});
+
+  @override
+  SplashPageState createState() => SplashPageState();
+}
+
+class SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 1), () {
+      // just delay for showing this slash page clearer because it too fast
+      checkSignedIn();
+    });
+  }
+
+  void checkSignedIn() async {
+    AuthProvider authProvider = context.read<AuthProvider>();
+    bool isLoggedIn = await authProvider.isLoggedIn();
+    if (isLoggedIn) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const HomePage()),
+      );
+      return;
+    }
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const LoginPage()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // Image.asset(
+            //   "images/app_icon.png",
+            //   width: 100,
+            //   height: 100,
+            // ),
+            const SizedBox(height: 20),
+            Container(
+              width: 20,
+              height: 20,
+              child: const CircularProgressIndicator(
+                  color: ColorConstants.themeColor),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
